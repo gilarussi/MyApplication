a0c352f0d08313dacfdad45efb3f18c7ccc271c4
gilarussi <gil2005.ar@gmail.com>
2022-11-10 09:05:02 +0200

    3 update, email sender 3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -25,10 +26,18 @@ import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
+
     private SwipeAdapter adapter;
     private List<Integer> list;
     Koloda koloda; //זה Class של הItem שאותו אנחנו מחליקים
+
+
+    private static final String TAG = "Swipe Position";
+    private float x1, x2, y1, y2;
+    private static int MIN_DISTANCE = 150;
+    private GestureDetector gestureDetector;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,54 +49,117 @@ public class MainActivity extends AppCompatActivity {
         adapter = new SwipeAdapter(this, list);
         koloda.setAdapter(adapter);
 
+
+        this.gestureDetector = new GestureDetector(MainActivity.this,this);
+
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
 
-        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction)
-        {                   //פעולה ששולחת אימייל
-            try {
-            String stringSenderEmail = "gil2005.ar@gmail.com";
-            String stringReceiverEmail = "sniram210@gmail.com";
-            String stringPasswordSenderEmail = "pomodoro28";
-            String stringHost = "smtp.gmail.com";
-            Properties properties = System.getProperties();
-            properties.put("mail.smtp.host", stringHost);
-            properties.put("mail.smtp.port", "465");
-            properties.put("mail.smtp.ssl.enable", "true");
-            properties.put("mail.smtp.auth", "true");
+        gestureDetector.onTouchEvent(event);
+        switch (event.getAction())
+        {
+            //starting to swipe time gesture
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                y1 = event.getY();
+                break;
 
-            javax.mail.Session session = Session.getInstance(properties, new Authenticator() {
-                @Override
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication(stringSenderEmail, stringPasswordSenderEmail);
-                }
-            });
-            MimeMessage mimeMessage = new MimeMessage(session);
+            //ending time swipe gesture
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                y2 = event.getY();
 
-                mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(stringReceiverEmail));
+                //getting value for horizontal swipe
+                float valueX = x2 - x1;
 
-                mimeMessage.setSubject("Android App Email");
-                mimeMessage.setText("Hello, I want to work for you");
+                //getting value for vertical swipe
+                float valueY = y2 - y1;
 
-                Thread thread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
+                if (Math.abs(valueX) > MIN_DISTANCE)
+                {
+                    //detect left to right swipe
+                    if (x2 > x1)
+                    {
                         try {
-                            Transport.send(mimeMessage);
-                        } catch (MessagingException e) {           //שולח את ההודעה
+                            String stringSenderEmail = "gil2005.ar@gmail.com";
+                            String stringReceiverEmail = "sniram210@gmail.com";
+                            String stringPasswordSenderEmail = "pomodoro28";
+                            String stringHost = "smtp.gmail.com";
+                            Properties properties = System.getProperties();
+                            properties.put("mail.smtp.host", stringHost);
+                            properties.put("mail.smtp.port", "465");
+                            properties.put("mail.smtp.ssl.enable", "true");
+                            properties.put("mail.smtp.auth", "true");
+
+                            javax.mail.Session session = Session.getInstance(properties, new Authenticator() {
+                                @Override
+                                protected PasswordAuthentication getPasswordAuthentication() {
+                                    return new PasswordAuthentication(stringSenderEmail, stringPasswordSenderEmail);
+                                }
+                            });
+                            MimeMessage mimeMessage = new MimeMessage(session);
+
+                            mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(stringReceiverEmail));
+
+                            mimeMessage.setSubject("Android App Email");
+                            mimeMessage.setText("Hello, I want to work for you");
+
+                            Thread thread = new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    try {
+                                        Transport.send(mimeMessage);
+                                    } catch (MessagingException e) {           //שולח את ההודעה
+                                        e.printStackTrace();
+                                    }
+                                }
+                            });
+                            thread.start();
+                        } catch (AddressException e) {
+                            e.printStackTrace();
+                        } catch (MessagingException e) {
                             e.printStackTrace();
                         }
                     }
-                });
-                thread.start();
-            } catch (AddressException e) {
-                e.printStackTrace();
-            } catch (MessagingException e) {
-                e.printStackTrace();
-            }
-
+                }
         }
 
 
+
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
 
 }
